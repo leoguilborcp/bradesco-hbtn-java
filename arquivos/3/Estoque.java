ab50213b6c4c6df378d431dba014f968ed07d88e
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-08-22 22:50:39 -0300

    (OPC) - Java - File Handling 3. Gerenciamento de Estoque com Arquivos

---
+++ arquivos/3/Estoque.java
@@ -0,0 +1,53 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Estoque {
+    private List<Produto> produtos = new ArrayList<>();
+
+    public Estoque(String fileName) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String linha = reader.readLine();
+            while (linha != null) {
+                String[] dados = linha.split(",");
+                Produto produto = new Produto(Integer.parseInt(dados[0]), dados[1], Integer.parseInt(dados[2]), Double.parseDouble(dados[3]));
+                produtos.add(produto);
+                linha = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        
+    }
+
+    public void adicionarProduto(String nome, int quantidade, double preco) {
+
+        Produto produto = new Produto(gerarId(), nome, quantidade, preco);
+        produtos.add(produto);
+    }
+
+    public void excluirProduto(int id) {
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    public void atualizarQuantidade(int idAtualizar, int novaQuantidade) {
+        for (Produto produto : produtos) {
+            if (produto.getId() == idAtualizar) {
+                produto.setQuantidade(novaQuantidade);
+                break;
+            }
+        }
+    }
+    public void exibirEstoque(){
+        for (Produto produto : produtos) {
+            System.out.println(produto.toString());
+        }
+    }
+    private int gerarId() {
+        return (produtos.size() > 0 ? produtos.get(produtos.size()-1).getId() : 0) + 1;
+    }
+}
