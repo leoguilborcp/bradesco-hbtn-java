f90e2aa700609fae7b6a9edc03f0d2cbf1575319
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-08-22 21:52:34 -0300

    (OPC) - Java - File Handling 0. Escrevendo arquivos

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -5,24 +5,27 @@ import java.io.BufferedWriter;
 
 
  public class FileWritingExercise {
-     public static void main(String[] args)  throws IOException {
+     public static void main(String[] args)  {
          Scanner scanner = new Scanner(System.in);
 
 
          // Solicita o nome do arquivo
          System.out.print("Digite o nome do arquivo (com extensão .txt): ");
          String fileName = scanner.nextLine();
-
+        try{
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
              System.out.println("Digite o conteúdo a ser escrito no arquivo (digite 'sair' para finalizar):");
              String linha;
-             while (!(linha = scanner.nextLine()).equalsIgnoreCase("sair")) {
+             while (!(linha = scanner.nextLine()).trim().equalsIgnoreCase("sair")) {
                  writer.write(linha);
                  writer.newLine();
              }
          
          
          writer.close();
+         } catch (IOException e) {
+             System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+         }
 
          scanner.close();
      }
