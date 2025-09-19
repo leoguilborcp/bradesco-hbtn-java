60a01ec2f3753cf58337cdcf1d113aed1c6f4fd7
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-09-19 20:51:34 -0300

    Java - Streams, Collectors e Optional 4. Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Consulta.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst().orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produtos) {
+        return produtos.stream()
+                .map(produto -> {
+                    if (produto.getCategoria() == CategoriaProduto.ELETRONICO) {
+                        produto.setPreco(produto.getPreco() * 0.85);
+                    }
+                    return produto;
+                })
+                .collect(Collectors.toList());
+    }
+}
