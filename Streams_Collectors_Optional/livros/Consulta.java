363a11f82ddc107ad3aaa7db8c8b753a706025fd
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-09-19 20:37:24 -0300

    Java - Streams, Collectors e Optional 0. Relatórios Livros

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+}
