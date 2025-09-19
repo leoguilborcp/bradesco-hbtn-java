196d732ee470c767ab18efaa9d5a0a9987073b45
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-09-19 20:46:54 -0300

    Java - Streams, Collectors e Optional 3. Relatório Pedidos com Eletrônicos

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Consulta {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst().orElse(null);
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {
+        return pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream()
+                        .anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO))
+                .collect(Collectors.toList());
+    }
+}
