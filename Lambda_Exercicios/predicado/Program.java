23467993d3e765c5596666341a23efcd288cfe30
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-09-19 19:26:52 -0300

    Java - Lambda Expressions - Exercícios de fixação 1. Consultar Produtos com Predicado

--- Lambda_Exercicios/predicado/Program.java
+++ Lambda_Exercicios/predicado/Program.java
@@ -1,4 +1,6 @@
-import java.util.*;
+    import java.lang.reflect.Method;
+    import java.lang.reflect.Parameter;
+    import java.util.*;
     import java.util.function.*;
         import java.util.Locale;
 
@@ -33,11 +35,21 @@ import java.util.*;
                 p -> p.getTipo() == TiposProduto.COZINHA);   
 
             List<Produto> produtosBaixoEstoque = ConsultaProdutos.filtrar(produtos, 
-                p -> p.getQuantidadeEmEstoque() <= 10);        
+                p -> p.getQuantidadeEmEstoque() <= 10);      
+
+            try
+            {
+                Method method = ConsultaProdutos.class.getMethod("filtrar", List.class, Predicate.class);
+                if (method != null) {
+                    System.out.println("Metodo encontrado");
+                }
+            } catch(NoSuchMethodException ex) {
+                System.out.println("Metodo nao encontrado");
+            }
 
             System.out.println(produtosPrecoMaiorQue100);
             System.out.println(produtosPesoMenorOuIgual1600);
             System.out.println(produtosCozinha);
             System.out.println(produtosBaixoEstoque);
         }
-    }
+    } 
