b9fdc7153dcda0737fcb86ec8893d878b32d52a3
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-10-06 00:08:05 -0300

    Java - Collectors 2. Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class ConsultaPessoas {
+    static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+        static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+    return pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+    static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+    return pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo,Collectors.filtering(
+            p -> p.getIdade() > 40, 
+            Collectors.toList()     
+        )));
+}
+}
---
+++ 0x19/acima_40/Pessoa.java
@@ -0,0 +1,43 @@
+import java.util.Locale;
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    // Getters e Setters
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+         Locale ptBR = new Locale("pt", "BR");
+        return String.format(ptBR,"[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
+    }
+}
