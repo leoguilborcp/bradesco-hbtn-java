1f89748804b21b8b42564b155cd71f1553d45c27
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-10-12 13:19:53 -0300

    Java - Streams, Collectors e Optional - Exercícios de fixação -1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class ConsultaPessoas {
+    static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+        static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+    return pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+    static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+    return pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo,Collectors.filtering(
+            p -> p.getIdade() > 40, 
+            Collectors.toList()     
+        )));
+    }
+    static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(() -> new TreeSet<>())));
+    }
+    static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+    return pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+}
